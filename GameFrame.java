49018add59c9d849746fd73a03191fac8e94b999
Alexander-Lao <alexander.lao@student.tdsb.on.ca>
2022-05-31 17:44:11 -0400

    Removed unnecessary imports

--- GameFrame.java
+++ GameFrame.java
@@ -6,9 +6,6 @@ Runs the constructor in GamePanel class
 import java.awt.*;
 import javax.swing.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import javax.imageio.ImageIO;
 
 public class GameFrame extends JFrame{
 
