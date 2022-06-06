5ae0dbffedb4580afba0ce4aa1b3f76a84cd5af8
Alexander-Lao <alexander.lao@student.tdsb.on.ca>
2022-06-06 17:40:17 -0400

    constricted grid, added current block image, added helpful comments for aaron to understand since my code is quite messy

--- LevelMaker.java
+++ LevelMaker.java
@@ -15,6 +15,8 @@ public class LevelMaker extends JPanel{
     private int mouseBlockY;
     private int mouseBlockXWithShifts;
     private int screenShifts;
+    private boolean ifNone;
+    private int xMoves;
 
     public int[][] map = new int[100][500];
 
@@ -24,6 +26,24 @@ public class LevelMaker extends JPanel{
         mouseBlockY = 0;
         mouseBlockXWithShifts = 0;
         screenShifts = 0;
+
+
+        /*These current positions are temporary, I did it like this because I didnt want to type each one out
+        I plan to keep the spacing the same but change the order of how the blocks are displayed
+        Currently:
+        1 2 3
+        4 5 6
+        7 8 9
+        a b c
+        d
+        
+        Plan:
+        9 2 3
+        8 1 4
+        7 6 5
+        b c
+        a d
+        */
 
         //Row 1
         for(int i = 0; i < 3; i++){
@@ -70,11 +90,11 @@ public class LevelMaker extends JPanel{
         g2.setStroke(new BasicStroke(1));
 
         //Grid Lines
-        for(int i = 0; i < GamePanel.GAME_WIDTH/Maps.blockSize; i++){
+        for(int i = 0; i < GamePanel.GAME_WIDTH/Maps.blockSize - 6; i++){
             g2.drawLine(i * Maps.blockSize, 0, i * Maps.blockSize, GamePanel.GAME_HEIGHT);
         }
         for(int i = 0; i < GamePanel.GAME_HEIGHT/Maps.blockSize; i++){
-            g2.drawLine(0, i * Maps.blockSize, GamePanel.GAME_WIDTH, i * Maps.blockSize);
+            g2.drawLine(0, i * Maps.blockSize, GamePanel.GAME_WIDTH - 7*50, i * Maps.blockSize);
         }
 
         //Shown editor blocks on the side
@@ -82,14 +102,31 @@ public class LevelMaker extends JPanel{
             g.drawImage(GameFrame.blocks[i], pos[i][0], pos[i][1], null);
         }
 
+        //Draw current block display
+        g.setFont(new Font("TimesRoman", Font.PLAIN, 40));
+        g.drawString("Current:", GamePanel.GAME_WIDTH - (6)*Maps.blockSize, 13*Maps.blockSize - 10);
+        //Draw empty rectange for no block selected
+        if(currentBlockNumber == 0){
+            g.drawRect(GamePanel.GAME_WIDTH - (2)*Maps.blockSize, 12*Maps.blockSize, Maps.blockSize, Maps.blockSize);
+        }
+        //Draw image of current block otherwise
+        else{
+            g.drawImage(GameFrame.blocks[currentBlockNumber-1], GamePanel.GAME_WIDTH - (2)*Maps.blockSize, 12*Maps.blockSize, null);
+        }
+ 
     }
 
     public void mousePressed(MouseEvent e){
-        if(mouseBlockX >= GamePanel.GAME_WIDTH/Maps.blockSize - 6){
+        ifNone = true;
+        if(mouseBlockX >= GamePanel.GAME_WIDTH/Maps.blockSize - 7){
             for(int i = 0; i < pos.length; i++){
                 if(hover[i]){
                     currentBlockNumber = i + 1;
+                    ifNone = false;
                 }
+            }
+            if(ifNone){
+                currentBlockNumber = 0;
             }
         }
         else{
@@ -110,7 +147,7 @@ public class LevelMaker extends JPanel{
     }
 
     public void mousePosition(int x, int y){
-        int xMoves = x + screenShifts;
+        xMoves = x + screenShifts;
         for(int i = 0; i < pos.length; i++){
             if(x > pos[i][0] && x < pos[i][0] + Maps.blockSize && y > pos[i][1] && y < pos[i][1] + Maps.blockSize){
                 hover[i] = true;
@@ -126,13 +163,17 @@ public class LevelMaker extends JPanel{
     }
 
     private void displayMap(Graphics g) {
-        g.drawImage(GameFrame.backgroundImage[1], 0, 0, this);
+        //parallax (the background moves)
+        g.drawImage(GameFrame.backgroundImage[1], (int)(-screenShifts/GamePanel.parallaxRatio % GamePanel.GAME_WIDTH), 0, GamePanel.GAME_WIDTH, GamePanel.GAME_HEIGHT, null);
+        g.drawImage(GameFrame.backgroundImage[1], (int)(-screenShifts/GamePanel.parallaxRatio % GamePanel.GAME_WIDTH) + GamePanel.GAME_WIDTH - 1, 0, GamePanel.GAME_WIDTH, GamePanel.GAME_HEIGHT, null);
 
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[0].length; j++) {
                 int pos = map[i][j];      
-
-                if (pos != 0) {
+                
+                //These conditions are to not draw anything that isint on the board
+                if (pos != 0 && j < GamePanel.GAME_WIDTH/Maps.blockSize - 7 + screenShifts/Maps.blockSize
+                && j > screenShifts/Maps.blockSize - 1) {
                     g.drawImage(GameFrame.blocks[pos-1], j * Maps.blockSize - (int) (screenShifts), i * Maps.blockSize, null);
                 }
             }
